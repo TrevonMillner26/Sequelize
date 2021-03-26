bacc4e06cf408134d3425682f83bf6a647d83c2f
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-03-26 19:34:15 -0400

    Still working on the public folder

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const staticFolder = 'public';
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/public/', express.static(staticFolder));
 
+const staticFolder = 'public';
+app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 
 async function bootServer() {
