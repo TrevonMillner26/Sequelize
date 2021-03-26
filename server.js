c8120628f5d69a75d5a113ec20d4a673ef022f32
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-03-26 18:27:19 -0400

    Update: still trying to create public folder

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
 
-const _dirname = path.resolve();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,7 +9,7 @@ const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(staticFolder));
+app.use('/public/', express.static(staticFolder));
 
 app.use('/api', apiRoutes);
 
