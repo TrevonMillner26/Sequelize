6c2e9870cac7613c78110e2632cbb1f2a1edaca6
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-05-04 11:31:13 -0400

    Fixing the script and making sure the chart displays the proper data

--- public/script.js
+++ public/script.js
@@ -1,53 +1,45 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-console */
-/* eslint-disable camelcase */
-/* eslint-disable max-len */
-
 function randInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
+}
+function randMeals(data) {
+  const random_meals = [];
+  for (i = 0; i < 10; i++) {
+    const current_random_meal = randInt(data.length - 1);
+    random_meals.push(data[current_random_meal]);
+    data.splice(current_random_meal, 1);
   }
-  function randMeals(data) {
-    const random_meals = [];
-    for (i = 0; i < 10; i++) {
-      const current_random_meal = randInt(data.length - 1);
-      random_meals.push(data[current_random_meal]);
-      data.splice(current_random_meal, 1);
-    }
-    return random_meals;
-  }
-  
-  async function dataHandler() {
-    // getting data from the api
-    const request = await fetch('/api/dining');
-    const api_data = await request.json();
-    const { data } = api_data;
-    const table = document.querySelector('.table');
-  
-    console.log(data);
-  
-    data.forEach((element) => {
-      console.log(element.hall_name);
-      console.log(element.hall_address);
-      console.log(element.hall_id);
-      const appendItem = document.createElement('tr');
-      appendItem.innerHTML = `<td>${element.hall_id}</td><td>${element.hall_name}</td><td>${element.hall_address}</td>`;
-      table.append(appendItem);
-    });
-  }
-  
-    const randMealList = randMeals(api_data);
-  
-    for (i = 0; i < randMealList.length; i++) {
-      element = randMealList[i];
-      const nameRequest = await fetch(`/api/meals/${element.meal_id}`);
-      const titleData = await nameRequest.json();
-      console.log(titleData);
+  return random_meals;
+}
+
+async function dataHandler() {
+  // getting data from the api
+  const request = await fetch('/api/dining');
+  const api_data = await request.json();
+  const { data } = api_data;
+  const table = document.querySelector('.table');
+
+  console.log(data);
+
+  data.forEach((element) => {
+    console.log(element.hall_name);
+    console.log(element.hall_address);
+    console.log(element.hall_id);
+    const appendItem = document.createElement('tr');
+    appendItem.innerHTML = `<td>${element.hall_id}</td><td>${element.hall_name}</td><td>${element.hall_address}</td>`;
+    table.append(appendItem);
+  });
+
+  const randMealList = randMeals(api_data);
+
+  for (i = 0; i < randMealList.length; i++) {
+    element = randMealList[i];
+    const nameRequest = await fetch(`/api/meals/${element.meal_id}`);
+    const titleData = await nameRequest.json();
+    console.log(titleData);
 
     const chart = new CanvasJS.Chart('chartContainer', {
       title: {
-        text: 'Macro Data'
+        text: 'Meal Macro Information'
       },
       data: macro_data
     });
