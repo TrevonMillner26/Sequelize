0edce14bf2628450a331036e3bef308eb95ef9a5
TrevonMillner26 <78122883+TrevonMillner26@users.noreply.github.com>
2021-05-11 19:01:01 -0400

    Redid the entire script.js file

--- public/script.js
+++ public/script.js
@@ -1,54 +1,29 @@
-function randInt(max) {
-  return Math.floor(Math.random() * max);
-}
-function randMeals(data) {
-  const random_meals = [];
-  for (i = 0; i < 10; i++) {
-    const current_random_meal = randInt(data.length - 1);
-    random_meals.push(data[current_random_meal]);
-    data.splice(current_random_meal, 1);
-  }
-  return random_meals;
-}
+async function dining(){
+  const requestData = await fetch('/api/dining');
+  const diningData = await requestData.json();
+  const arrayData = diningData.data;
+  console.log(arrayData);
+  console.table(diningData);
+  const targetTable = document.querySelector('.table');
 
-async function dataHandler() {
-  // getting data from the api
-  const request = await fetch('/api/dining');
-  const api_data = await request.json();
-  const { data } = api_data;
-  const table = document.querySelector('.chartContainer');
 
-  console.log(data);
 
-  data.forEach((element) => {
-    console.log(element.hall_name);
-    console.log(element.hall_address);
-    console.log(element.hall_id);
-    const appendItem = document.createElement('tr');
-    appendItem.innerHTML = `<td>${element.hall_id}</td><td>${element.hall_name}</td><td>${element.hall_address}</td>`;
-    table.append(appendItem);
+  arrayData.forEach((element) => {
+    const appendElement = document.createElement('tr');
+    appendElement.innerHTML = `
+      <td>${element.hall_id}</td>
+      <td>${element.hall_name}</td>
+      <td>${element.hall_address}</td>
+    </tr>
+  `;
+    targetTable.append(appendElement); 
   });
+}
 
-  const randMealList = randMeals(api_data);
+async function windowActions() {
+  console.log('loaded window');
+  const data = await dining();
+  console.table(data);
+}
 
-  for (i = 0; i < randMealList.length; i++) {
-    element = randMealList[i];
-    const nameRequest = await fetch(`/api/meals/${element.meal_id}`);
-    const titleData = await nameRequest.json();
-    console.log(titleData);
-
-    const chart = new CanvasJS.Chart('chartContainer', {
-      title: {
-        text: 'Meal Macro Information'
-      },
-      data: macro_data
-    });
-    chart.render();
-  }
-  
-  async function windowActions() {
-    await dataHandler();
-    await macrosData();
-  }
-  
-  window.onload = windowActions;
+window.onload = windowActions;
